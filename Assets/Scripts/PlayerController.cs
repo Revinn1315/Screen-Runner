6989b5a3d665e0ef40d9fa6179bc34d6b0ef221c
Revinn1315 <revinn1315@gmail.com>
2025-07-25 21:55:50 +0530

    Implemented player flipping but is not working
    On player control, Check line 67 to 83 and line 118 to 132.
    
    This code as directed in the tutorial should make the character flip but for some reason the toggle is not working.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,41 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.Rendering.DebugUI;
+
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 5f;
+    public float walkSpeed = 300f;
+    public float runSpeed = 500f;
     Vector2 moveInput;
-    public InputActionReference move;
+
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (IsMoving)
+            {
+                if (IsRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            }
+            else
+            {
+                // Idle speed is 0
+                return 0;
+            }
+        }
+    }
 
     [SerializeField] private bool _isMoving = false;
 
-    public bool isMoving 
+    public bool IsMoving 
     { 
         get 
         {
@@ -19,15 +43,15 @@ public class PlayerController : MonoBehaviour
         }
         set
         {
-            isMoving = value;
-            animator.SetBool("isMoving", isMoving);
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
 
         }
     }
 
     [SerializeField] private bool _isRunning = false;
 
-    public bool isRunning
+    public bool IsRunning
     {
         get
         {
@@ -36,7 +60,27 @@ public class PlayerController : MonoBehaviour
         set
         {
             _isRunning = value;
-            animator.SetBool("isRunning", isRunning);
+            animator.SetBool("isRunning", value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight 
+    { 
+        get 
+        { 
+            return _isFacingRight; 
+        } 
+        private set 
+        {
+            //Flip only if value is new
+            if (_isFacingRight != value)
+            {
+                //Flip the local scale to make the player face the opposite directions
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
         }
     }
 
@@ -51,7 +95,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        move.action.performed += OnMove;
+            
     }
 
     // Update is called once per frame
@@ -62,25 +106,41 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * walkSpeed * Time.fixedDeltaTime, rb.linearVelocityY);
+        rb.linearVelocity = new Vector2(moveInput.x * CurrentMoveSpeed * Time.fixedDeltaTime, rb.linearVelocityY);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
 
-        isMoving = moveInput != Vector2.zero;
+        IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            //Face the right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && !IsFacingRight)
+        {
+            //Face the left
+            IsFacingRight = false;
+        }
     }
 
     public void OnRun(InputAction.CallbackContext context)
     {
         if (context.started)
         {
-            isRunning = true;
+            IsRunning = true;
         }
         else if(context.canceled)
         {
-            isRunning = false;
+            IsRunning = false;
         }
     }
 }
